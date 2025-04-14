9490b6033deadf14f468905de83fa11166a6fbb1
scarjo11 <sezzle-wave@live.fr>
2025-04-14 09:36:22 +0200

    GET route, POST route, and created a schema with mongoose

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Thing = require('./models/Thing')
+
+const app = express();
+
+mongoose.connect('mongodb+srv://jessie:reyez@cluster0.6hi5ahv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Express prend toutes les requêtes qui ont comme Content-Type  application/json  et met à disposition leur  body  
+// directement sur l'objet req
+app.use(express.json());
+
+// pour permettre à l'application d'accéder à l'API et éviter l'erreur CORS, on configure des headers spécifiques sur l'objet réponse
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,58 @@
 // import du package http de Node
 const http = require('http');
+const app = require('./app')
 
+// dire sur quel port Express va tourner
+// app.set('port', process.env.PORT || 3000)
 // création du serveur à partir du package http. On peut lui envoyer des requêtes
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur');
-});
+// const server = http.createServer(app);
 
 // on écoute le serveur usr port 3000, ou VarEnv si port n'est pas dispo. On attend les requêtes envoyées
-server.listen(process.env.PORT || 3000);
+// server.listen(process.env.PORT || 3000);
+
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
